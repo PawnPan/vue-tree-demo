3c2fa7d3eb7e7bfa5a2854f55c9d8475dd4bc5b7
panruopeng <alhprp@gmail.com>
2019-06-01 00:06:37 +0800

    add dispatch and broadcast

--- src/main.js
+++ src/main.js
@@ -3,6 +3,22 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-new Vue({
+Vue.prototype.$dispatch = function(eventName, args) {
+  this.$emit(eventName, args);
+  if (this.$parent) {
+    this.$parent.$dispatch(eventName, args);
+  }
+};
+
+Vue.prototype.$broadcast = function(eventName, args) {
+  this.$emit(eventName, args);
+  if (this.$children && this.$children.length) {
+    for (let i of this.$children) {
+      i.$broadcast(eventName, args);
+    }
+  }
+};
+
+window.app = new Vue({
   render: h => h(App),
 }).$mount('#app')
